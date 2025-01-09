ed4b39ee1d8431339ed9d92ae6ea662bb97296ac
Darshan <darshanp@vayavyalabs.com>
2025-01-09 17:30:00 +0530

    Added check for HCIVERSION

--- test.c
+++ test.c
@@ -6,6 +6,7 @@
 // Define offsets for HCSPARAMS1 and HCCPARAMS1 registers
 #define HCSPARAMS1_OFFSET 0x04
 #define HCCPARAMS1_OFFSET 0x10
+#define HCIVERSION_OFFSET 0x02
 
 // Function to read a 32-bit value from a memory-mapped register
 static inline uint32_t mmio_read32(uintptr_t addr) {
@@ -14,7 +15,7 @@ static inline uint32_t mmio_read32(uintptr_t addr) {
 
 int main() {
     // Pointers to the registers
-    volatile uint32_t hcsp1_value = 0, hccp1_value = 0;
+    volatile uint32_t hcsp1_value = 0, hccp1_value = 0, hciver_value = 0;
 
     // Read HCSPARAMS1 register
     hcsp1_value = mmio_read32(XHCI_BASE_ADDR + HCSPARAMS1_OFFSET);
@@ -22,10 +23,13 @@ int main() {
     // Read HCCPARAMS1 register
     hccp1_value = mmio_read32(XHCI_BASE_ADDR + HCCPARAMS1_OFFSET);
 
+    hciver_value = mmio_read32(XHCI_BASE_ADDR + HCIVERSION_OFFSET);
+
     // Variables can be checked using GDB or printed if serial output is
     // available
     hcsp1_value = hcsp1_value;
     hccp1_value = hccp1_value;
+    hciver_value = hciver_value;
     while (1);  // Infinite loop to keep the program running
     return 0;
 }
