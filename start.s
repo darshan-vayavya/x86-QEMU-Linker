dc66b3549c44b9a9661c58d21ff39a49404b5308
Darshan <darshanp@vayavyalabs.com>
2025-02-05 15:42:02 +0530

    Added VGA Display message on startup and completion

--- start.s
+++ start.s
@@ -21,54 +21,6 @@ _boot_grub:
     .long CHECKSUM
 # Multiboot headers - setup done
 
-
-# PCI xHCI Related Info
-# QEMU Specific PCI Base Address
-.set PCI_DEVICE_ADDRESS_BASE, 0x80000000
-
-# Bus Number of the connected xHCI device
-.set xHCI_BUS, 0
-# Device Number
-.set xHCI_DeviceNum, 5
-# Function Number
-.set xHCI_Function, 0
-
-# xHCI Device Address without Register Offsets
-.set xHCI_PCI_ADDR, PCI_DEVICE_ADDRESS_BASE | (xHCI_BUS << 16) | (xHCI_DeviceNum << 11) | (xHCI_Function << 8)
-
-# Offsets of different Registers in PCI Space
-.set PCI_VID_OFFSET, 0x00       # Vendor ID (16 bits)
-.set PCI_DID_OFFSET, 0x02       # Device ID (16 bits)
-.set PCI_CMD_OFFSET, 0x04       # Command Register (16 bits)
-.set PCI_STATUS_OFFSET, 0x06    # Status Register (16 bits)
-.set PCI_REV_ID_OFFSET, 0x08    # Revision ID (8 bits)
-.set PCI_CLASS_CODE_OFFSET, 0x09 # Class Code (24 bits: Prog IF, Subclass, Base Class)
-.set PCI_HEADER_TYPE, 0x0E      # Header Type (8 bits)
-.set PCI_BIST, 0x0F             # Built-in Self-Test (8 bits)
-
-.set PCI_BAR_0_OFFSET, 0x10     # Base Address Register 0 (32 bits or 64 bits)
-.set PCI_BAR_1_OFFSET, 0x14     # Base Address Register 1 (32 bits or 64 bits)
-
-
-# Final Calculated Addresses
-.set xHCI_VID_ADDR,  xHCI_PCI_ADDR | PCI_VID_OFFSET
-.set xHCI_DID_ADDR,  xHCI_PCI_ADDR | PCI_DID_OFFSET
-.set xHCI_BAR0_ADDR, xHCI_PCI_ADDR | PCI_BAR_0_OFFSET
-.set xHCI_BAR1_ADDR, xHCI_PCI_ADDR | PCI_BAR_1_OFFSET
-.set xHCI_CMDR_ADDR, xHCI_PCI_ADDR | PCI_CMD_OFFSET
-.set xHCI_HEAD_ADDR, xHCI_PCI_ADDR | PCI_HEADER_TYPE
-
-# Macro to test value at a particular PCI address - result is in eax register
-.macro read_pci addr
-    movl \addr, %eax
-    movw $0xCF8, %dx            # PCI configuration address register
-    outl %eax, %dx              # Write to select PCI command register
-    movw $0xCFC, %dx            # PCI configuration data register
-    inl %dx, %eax               # Read PCI command register value
-    mfence
-.endm
-
-
 .section .text
 .global _start
 .code32
@@ -88,14 +40,22 @@ _start:
     # Initialize FPU
     fninit
 
+    # Display welcome message
+    call display_boot
+
     # Jump to main
     call main
 
 _end:
+    # Display end message
+    call display_end
+
+    # Close QEMU
     movw $0x2000, %ax
     movw $0x604, %dx
     outw %ax, %dx
     
+    # Safety barrier
     hlt
 
 .section .rodata
